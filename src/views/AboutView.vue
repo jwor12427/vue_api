11c91a01224dfa6b8806cc809ddbecbb9705398a
jwor12427 <jwor12427@gmail.com>
2022-11-15 23:27:00 +0900

    2022.11.15

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -7,16 +7,70 @@
 		<div class="about__inner">
 			<div class="container">
 				<div class="about01">
-					<img src="../assets/img/aboutImg01.png" alt="" class="about__img" />
-					<div class="about__desc"></div>
+					<img
+						src="../assets/img/aboutImg01.png"
+						alt="about이미지1"
+						class="about__img"
+					/>
+					<h3>
+						“세상에서 가장 어려운 일은 사람이 사람의 마음을 얻는 일이란다.”
+					</h3>
+					<div class="about__desc">
+						나의 가장 좋아하는 소설인 어린왕자에 나오는 말인 위의 구절은 언제나
+						인생을 한번씩 되돌아 보겠금 만드는 구절이다. 사람이 사람의 마음을
+						얻는것이 정말 어려운 일이라는 것은 점점 커가면서 비로소 나에게 크게
+						다가오는 일이 되었다. 어린왕자에서도 이 이야기의 질문과 대답을
+						하는것은 ‘나’이고, 그것을 질문하는 대상은 바로 어린왕자이다.
+						살아가면서 점점 하는일도 힘들어지고, 마음을 얻는것은 더욱 어려워
+						졌다. 하지만, 그럴수록 나는 노력을 하고 열심히 살아 가야하는 것을
+						배우게 되었다. 그런의미에서 새로운 도전에도 포기하지 않고, 열심히
+						배우며 살아가고 싶다.
+					</div>
 				</div>
 				<div class="about02">
-					<img src="" alt="" class="about__img" />
-					<div class="about__desc"></div>
+					<img
+						src="../assets/img/aboutImg02.jpg"
+						alt="about이미지2"
+						class="about__img"
+					/>
+
+					<div class="about__desc">
+						<h3>
+							“그렇지만 기다려야해...”<br />
+							“뭘 기다려?” <br />
+							“해가 지길 기다려야 한단 말이야”
+						</h3>
+						좋아하는 것을 얻을려면 기다림이 있어야 한다. 기다림은 때로는 힘들고,
+						지칠때도 있지만 그 후의 보상은 참으로 값진것이 많다. 좋아하는 것은
+						절대 그저 내게 다가 오지 않는다. 내가 행동을 하거나, 기다림을
+						실천해야지 비로소 얻을 수 있을 때가 많다. 때로는 힘들고, 지칠때도
+						있지만 기다림은 나를 포기하지 않는다. 내가 한 만큼 그만큼의 값진
+						것을 나에게 선물해준다. 그렇듯 내가 처음 코딩을 배울때에도 기다림이
+						있어야 했다. 나는 이제 이 일을 처음 하는 것이고, 그만큼 좋아하고
+						보람을 느낄 려면 기다림은 필수 였다. 그리고 이제 그 기다림이 결실을
+						조금씩 맺고 있다.
+					</div>
 				</div>
 				<div class="about03">
-					<img src="" alt="" class="about__img" />
-					<div class="about__desc"></div>
+					<img
+						src="../assets/img/aboutImg03.png"
+						alt="about이미지3"
+						class="about__img"
+					/>
+
+					<div class="about__desc">
+						<h3>
+							“다른 사람에게는 결코 열어주지 않는 문을 당신에게만 열어주는
+							<br />
+							사람이 있다면 그사람이야 말로... <br />
+							당신의 진정한 친구다.”
+						</h3>
+						나에게 새로운 길을 열어준 코딩은 이제 나의 친구가 되었다. 나에게
+						열어주지 않을 것 같던 문을 열어주었고, 이제 나는 그 문의 길을 들어갈
+						준비를 하는 사람이다. 처음에는 이 문이 과연 열릴까라는 생각을 하기도
+						했고, 의심도 했지만... 이제 문이 조금씩 열렸다는 것을 알고나니 더욱
+						힘차게 이문을 열고 나아가야 겠다는 생각을 하게 되었다.
+					</div>
 				</div>
 			</div>
 		</div>
@@ -39,3 +93,65 @@ export default {
 	},
 };
 </script>
+
+<style lang="scss">
+.about01 {
+	font-family: var(--font-kor2);
+	margin-bottom: 180px;
+	img {
+		width: 100%;
+		margin-bottom: 20px;
+	}
+	h3 {
+		font-size: 18px;
+		margin-bottom: 5px;
+	}
+	.about__desc {
+		width: 840px;
+		font-size: 16px;
+		line-height: 1.4;
+		word-break: keep-all;
+	}
+}
+.about02 {
+	display: flex;
+	justify-content: space-between;
+	font-family: var(--font-kor2);
+	margin-bottom: 250px;
+	.about__img {
+		width: 68%;
+		height: 1000px;
+	}
+	.about__desc {
+		width: 28%;
+		font-size: 16px;
+		line-height: 1.4;
+		word-break: keep-all;
+		h3 {
+			font-size: 18px;
+			margin-bottom: 5px;
+		}
+	}
+}
+.about03 {
+	display: flex;
+	flex-direction: row-reverse;
+	justify-content: space-between;
+	font-family: var(--font-kor2);
+	margin-bottom: 180px;
+	.about__img {
+		width: 48%;
+		height: 480px;
+	}
+	.about__desc {
+		width: 48%;
+		font-size: 16px;
+		line-height: 1.4;
+		word-break: keep-all;
+		h3 {
+			font-size: 18px;
+			margin-bottom: 5px;
+		}
+	}
+}
+</style>
--- src/views/UnsplashView.vue
+++ src/views/UnsplashView.vue
@@ -7,11 +7,24 @@
 		<section class="cont__refer">
 			<div class="container">
 				<div class="unsplash__inner">
-					<div class="unsplash__slider"></div>
+					<div class="unsplash__slider">
+						<swiper
+							:effect="'cards'"
+							:grabCursor="true"
+							:modules="modules"
+							class="mySwiper"
+						>
+							<swiper-slide v-for="slider in sliders" :key="slider.id"
+								><a :href="`https://unsplash.com/photos/${slider.id}`">
+									<img :src="slider.urls.regular" :alt="slider.id" /></a
+							></swiper-slide>
+							<swiper-slide>Slide 1</swiper-slide>
+						</swiper>
+					</div>
 					<div class="movie__search">
 						<div class="container">
 							<h2>검색하기</h2>
-							<form @submit.prevent="SearchSplashes()">
+							<form @submit.prevent="SearchSplash()">
 								<input
 									type="search"
 									id="search"
@@ -24,9 +37,12 @@
 					</div>
 					<div class="unsplash__images">
 						<ul>
-							<li v-for="splash in splashes" :key="splash.id">
-								<a :href="`https://unsplash.com/photos/${splash.id}`">
-									<img :src="splash.urls.regular" :alt="splash.id" />
+							<li v-for="splash in splashes" :key="splash.links">
+								<a :href="splash.links.download">
+									<img
+										:src="splash.urls.regular"
+										:alt="splash.urls.alt_description"
+									/>
 								</a>
 							</li>
 						</ul>
@@ -44,6 +60,12 @@ import HeaderCont from '@/components/HeaderCont.vue';
 import FooterCont from '@/components/FooterCont.vue';
 import TitleCont from '@/components/TitleCont.vue';
 import ContactCont from '@/components/ContactCont.vue';
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { EffectCards } from 'swiper';
+
+import 'swiper/css';
+
+import 'swiper/css/effect-cards';
 import { ref } from 'vue';
 export default {
 	components: {
@@ -51,27 +73,39 @@ export default {
 		FooterCont,
 		TitleCont,
 		ContactCont,
+		Swiper,
+		SwiperSlide,
 	},
 
 	setup() {
 		const splashes = ref([]);
 		const sliders = ref([]);
-		const search = ref([]);
+		const search = ref('wallpaper');
 
-		const SearchSplashes = async () => {
+		const SearchSplash = async () => {
+			// 서치이미지
 			await fetch(
-				`https://api.unsplash.com/search/photos?client_id=ARu1VkatUtWaDHDwjkYnEyEK_OAVZ-LOCf-cTrHBtQg&query=${search.value}&count=20`,
+				`https://api.unsplash.com/search/photos?client_id=ARu1VkatUtWaDHDwjkYnEyEK_OAVZ-LOCf-cTrHBtQg&query=${search.value}&per_page=24`,
 			)
 				.then(response => response.json())
-				// .then(result => console.log(result))
 				.then(result => {
-					splashes.value = result;
+					console.log(result);
+					splashes.value = result.results;
 					search.value = '';
 				})
-				.then(error => console.log(error));
+				.catch(error => console.log('error', error));
 		};
-		SearchSplashes();
-		const RandomSplashes = async () => {
+		SearchSplash();
+		// const RandomSplashes = async () => {
+		// 	await fetch(
+		// 		'https://api.unsplash.com/photos/random?client_id=ARu1VkatUtWaDHDwjkYnEyEK_OAVZ-LOCf-cTrHBtQg&count=20&query=wallpaper',
+		// 	)
+		// 		.then(response => response.json())
+		// 		.then(result => (splashes.value = result))
+		// 		.then(error => console.log(error));
+		// };
+		// RandomSplashes();
+		const SliderSplashes = async () => {
 			await fetch(
 				'https://api.unsplash.com/photos/random?client_id=ARu1VkatUtWaDHDwjkYnEyEK_OAVZ-LOCf-cTrHBtQg&count=10',
 			)
@@ -79,13 +113,15 @@ export default {
 				.then(result => (sliders.value = result))
 				.catch(error => console.log(error));
 		};
-		RandomSplashes();
+		SliderSplashes();
 		return {
 			splashes,
 			search,
 			sliders,
-			SearchSplashes,
-			RandomSplashes,
+			SearchSplash,
+			// RandomSplashes,
+			SliderSplashes,
+			modules: [EffectCards],
 		};
 	},
 };
@@ -104,6 +140,23 @@ export default {
 				border-radius: 5px;
 			}
 		}
+	}
+}
+.unsplash__slider {
+	margin-bottom: 30px;
+	.swiper {
+		width: 300px;
+		height: 420px;
+	}
+
+	.swiper-slide {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		border-radius: 18px;
+		font-size: 22px;
+		font-weight: bold;
+		color: #fff;
 	}
 }
 </style>
